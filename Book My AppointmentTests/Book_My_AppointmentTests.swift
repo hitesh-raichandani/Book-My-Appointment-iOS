044af852127644dc2dfb9983804622b38028f3f0
hitesh <hiteshr13@gmail.com>
2016-11-13 02:12:04 -0800

    Initial Commit

---
+++ Book My AppointmentTests/Book_My_AppointmentTests.swift
@@ -0,0 +1,36 @@
+//
+//  Book_My_AppointmentTests.swift
+//  Book My AppointmentTests
+//
+//  Created by Hitesh Raichandani on 11/13/16.
+//  Copyright © 2016 Book My Appointment. All rights reserved.
+//
+
+import XCTest
+@testable import Book_My_Appointment
+
+class Book_My_AppointmentTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
